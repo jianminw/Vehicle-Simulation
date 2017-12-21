76c4695bdc9cfe4e611dbfc1c857502cfc82b410
Jianming Wang <jianminw@andrew.cmu.edu>
2017-12-21 09:42:10 -0500

    slip ratio working

---
+++ work/Pacejka_sf.c
@@ -0,0 +1,12 @@
+/*
+ * Generated S-function Target for model Pacejka. 
+ * This source file provides access to the generated S-function target
+ * for other models.
+ *
+ * Created: Thu Oct 12 11:48:54 2017
+ */
+
+#include "Pacejka_sf.h"
+#include "Pacejka_sfcn_rtw\Pacejka_sf.c"
+
+
---
+++ work/Pacejka_sf.h
@@ -0,0 +1,15 @@
+/*
+* Generated S-function Target for model Pacejka. 
+* This file provides access to the generated S-function target
+* export file for other models.
+*
+* Created: Thu Oct 12 11:48:54 2017
+*/
+
+#ifndef RTWSFCN_Pacejka_sf_H
+#define RTWSFCN_Pacejka_sf_H
+
+#include "Pacejka_sfcn_rtw\Pacejka_sf.h"
+  #include "Pacejka_sfcn_rtw\Pacejka_sf_private.h"
+
+#endif
